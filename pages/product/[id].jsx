c84b9ad15f9f8eb9e919b886fe0bb6008006947d
MGibson7 <MGibsonASC@gmail.com>
2022-06-17 12:16:11 -0400

    Added shipping selections

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,12 +4,14 @@ import { useState } from "react"
 
 const Product = () => {
     const[size, setSize] = useState(0);
+    const[ship, setShip] = useState("Free Ground");
     const hallItem = {
         id: 1,
         img: "/img/inferno.webp",
         name: "Inferno Pumpkin Skeleton",
         price: [299.99, 350.19, 450.21],
-        desc: "Gently used 12 foot inferno pumpkin skeleton originally from Home Depot"
+        desc: "Gently used 12 foot inferno pumpkin skeleton originally from Home Depot",
+        ship: ["Free Ground", "Express", "2 Day"]
     };
   return (
     <div className={styles.container}>
@@ -25,14 +27,15 @@ const Product = () => {
             <span className = {styles.price}>${hallItem.price[size]}</span>
             <p className = {styles.desc}>{hallItem.desc}</p>
             <h3>Choose Your Shipping</h3>
+            <h3>Current Selection: {hallItem.ship[size]}</h3>
             <div className={styles.sizes}>
-            <div className = {styles.size}>
-                <span className = {styles.number}>Free Ground</span>
+            <div className = {styles.size} onClick = {()=> setSize(0)}>
+                <span className = {styles.number} >Free Ground</span>
             </div>
-            <div className = {styles.size}>
+            <div className = {styles.size} onClick = {()=> setSize(1)}>
                 <span className = {styles.number}>Express</span>
             </div>
-            <div className = {styles.size}>
+            <div className = {styles.size} onClick = {()=> setSize(2)}>
                 <span className = {styles.number}>2 Day</span>
             </div>
 
